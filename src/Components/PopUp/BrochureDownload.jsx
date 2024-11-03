826db5cf7168de3811ad59d2c0405785842e8cbf
guptaarth87 <guptaarth87@gmail.com>
2024-11-03 21:10:38 +0530

    initial

--- src/Components/PopUp/BrochureDownload.jsx
+++ src/Components/PopUp/BrochureDownload.jsx
@@ -4,18 +4,18 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const BrochureDownload = () => {
   const handleDownload = () => {
-    const fileUrl = 'src/components/PopupDiv/Brochure.pdf';
+    const fileUrl = 'src/Components/PopUp/brochure.pdf';
     // const fileUrl = 'brochure.pdf';
     const link = document.createElement('a');
     link.href = fileUrl;
-    link.download = 'Brochure.pdf';
+    link.download = 'brochure.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
   return (
-    <button className="btn botton theme-grad" onClick={handleDownload}>
+    <button className="btn botton" onClick={handleDownload}>
       <FontAwesomeIcon icon={faDownload} /> Download Brochure
     </button>
   );
