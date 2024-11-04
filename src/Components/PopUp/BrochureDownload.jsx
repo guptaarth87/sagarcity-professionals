e91f035fd97c5a91965d5ba1fdfc87286dc28781
guptaarth87 <guptaarth87@gmail.com>
2024-11-04 10:59:18 +0530

    initial

--- src/Components/PopUp/BrochureDownload.jsx
+++ src/Components/PopUp/BrochureDownload.jsx
@@ -4,8 +4,7 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 const BrochureDownload = () => {
   const handleDownload = () => {
-    const fileUrl = 'src/Components/PopUp/brochure.pdf';
-    // const fileUrl = 'brochure.pdf';
+    const fileUrl = '/brochure.pdf'; // Direct path to public folder
     const link = document.createElement('a');
     link.href = fileUrl;
     link.download = 'brochure.pdf';
@@ -22,3 +21,29 @@ const BrochureDownload = () => {
 };
 
 export default BrochureDownload;
+
+
+// import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faDownload } from '@fortawesome/free-solid-svg-icons';
+
+// const BrochureDownload = () => {
+//   const handleDownload = () => {
+//     const fileUrl = 'src/Components/PopUp/brochure.pdf';
+//     // const fileUrl = 'brochure.pdf';
+//     const link = document.createElement('a');
+//     link.href = fileUrl;
+//     link.download = 'brochure.pdf';
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//   };
+
+//   return (
+//     <button className="btn botton" onClick={handleDownload}>
+//       <FontAwesomeIcon icon={faDownload} /> Download Brochure
+//     </button>
+//   );
+// };
+
+// export default BrochureDownload;
